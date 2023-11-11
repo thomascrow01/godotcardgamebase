139083493eaff6fd638374d5a98e1f42750aee26
thomas crow <thomascrow01@gmail.com>
2023-11-12 02:24:30 +1100

    looking into csv for godot

---
+++ cardgame/scripts/ReadCsv.gd
@@ -0,0 +1,37 @@
+#	ReadCsv.gd
+#   Written by Thomas Crow
+#	Creation date: 12/11/2023
+#	Last updated: 12/11/2023
+# Function to read csv
+
+extends Node
+
+# to read later 
+# https://docs.godotengine.org/en/stable/tutorials/assets_pipeline/importing_translations.html#:~:text=Godot%20will%20treat%20CSV%20files%20as%20translations%20by,runs%2C%20specified%20in%20project.godot%20%28or%20the%20project%20settings%29.
+
+# webrequest to get csv from url
+#https://docs.godotengine.org/en/stable/tutorials/networking/http_request_class.html
+
+
+
+#https://ask.godotengine.org/40566/how-can-i-import-a-csv-or-txt-file
+
+#var file = File.new()
+#file.open("res://resources/data/llangollen_aqueduct_trees.csv", file.READ)
+
+#var d = {}
+
+#while !file.eof_reached():
+#    var csv = file.get_csv_line ()
+
+#file.close()
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
