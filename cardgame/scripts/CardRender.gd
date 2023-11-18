b0f24768e2d1ce4d73a0217a224689572b7dc7dc
thomas crow <thomascrow01@gmail.com>
2023-11-18 21:49:15 +1100

    messing around a little bit yet again

--- cardgame/scripts/CardRender.gd
+++ cardgame/scripts/CardRender.gd
@@ -1,7 +1,7 @@
 #	CardRender.gd
 #   Written by Thomas Crow
 #	Creation date: 12/11/2023
-#	Last updated: 16/11/2023
+#	Last updated: 18/11/2023
 # This is a base for rendering cards
 
 extends Node
@@ -11,8 +11,45 @@ var node_hud : Node2D
 
 #var card : Card
 
-var hidden : bool
-var invisible : bool
+var hidden : bool # swap the details displayed on the card for something generic to hide information that should not be visible for gameplay reasons (cards in opponent's hands or cards that are face down)
+var invisible : bool # make the card invisible, use this for if the card is not meant to be visible at all
+
+func update_card() :
+	pass
+
+func toggle_hidden() :
+	set_hidden(!hidden)
+	
+func set_hidden(state : bool) :
+	hidden = state
+	
+	if hidden :
+		node_game.get_node("Label3D").visible = true
+		
+		# DESIGN BETTER WAY OF DOING THE IMAGE
+		# will have a field somewhere to have the generic face asset
+		var material : StandardMaterial3D = (node_game.get_node("MeshInstance3D") as MeshInstance3D).get_surface_override_material(0)
+		material.albedo_texture = load("res://icon.svg")
+	else :
+		node_game.get_node("Label3D").visible = false
+		
+		# ALSO MAKE FUNCTION FOR SETTING IMAGE
+		# decide on the Card.gd Key for the image asset
+		var material : StandardMaterial3D = (node_game.get_node("MeshInstance3D") as MeshInstance3D).get_surface_override_material(0)
+		material.albedo_texture = load("res://icon.svg")
+		
+		update_card()
+	
+func toggle_invisible() :
+	set_invisible(!invisible)
+	
+func set_invisible(state : bool) :
+	invisible = state
+	
+	if invisible :
+		node_game.visible = false
+	else :
+		node_game.visible = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -24,5 +61,5 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	(node_game as Node3D).transform.rotated(Vector3(1,0,0), 45.0)
+#func _process(delta):
+#	node_game.rotate(Vector3(0, 0, 0), 1) # ok I give up on this rotating stuff for now
--- cardgame/scripts/ReadCsv.gd
+++ cardgame/scripts/ReadCsv.gd
@@ -1,10 +1,10 @@
 #	ReadCsv.gd
 #   Written by Thomas Crow
 #	Creation date: 12/11/2023
-#	Last updated: 12/11/2023
+#	Last updated: 18/11/2023
 # Function to read csv
 
-extends Node
+#extends Node
 
 # to read later 
 # https://docs.godotengine.org/en/stable/tutorials/assets_pipeline/importing_translations.html#:~:text=Godot%20will%20treat%20CSV%20files%20as%20translations%20by,runs%2C%20specified%20in%20project.godot%20%28or%20the%20project%20settings%29.
@@ -15,16 +15,35 @@ extends Node
 
 
 #https://ask.godotengine.org/40566/how-can-i-import-a-csv-or-txt-file
+#https://ask.godotengine.org/148063/solved-how-do-you-do-file-new-in-4x
+func read_csv(csv_path : String) :
+	
+	#check if url
+	if csv_path.contains("http://") || csv_path.contains("https://"):
+		pass
+#		return get_asset_from_url(file_path)
+	
+	var file = FileAccess.open(csv_path, FileAccess.READ)
 
-#var file = File.new()
-#file.open("res://resources/data/llangollen_aqueduct_trees.csv", file.READ)
+	var d = {}
+	var columns = []
 
-#var d = {}
+	var first_line = true
 
-#while !file.eof_reached():
-#    var csv = file.get_csv_line ()
+	while !file.eof_reached():
+		var csv = file.get_csv_line ()
 
-#file.close()
+		if first_line :
+			for col in csv :
+				d[col] = []
+				columns.append(col)
+			continue
+
+		for col in csv :
+			for column in columns :
+				d[column].append(col)
+
+	file.close()
 
 
 # Called when the node enters the scene tree for the first time.
